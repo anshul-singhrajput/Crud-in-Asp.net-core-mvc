7b973db7e8f03a3e454a7e9d75e34ecc2d9543a1
anshul-singhrajput <143593203+anshul-singhrajput@users.noreply.github.com>
2024-05-10 12:39:38 +0530

    Initial commit

---
+++ CRUD operations/CRUD operations/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD_operations.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_operations.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; } 
+    }
+}
